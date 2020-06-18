47e618c201f9bbbda64873f68df38623753d6c19
Jeremy W <jtwillo51@gmail.com>
2020-06-17 18:11:18 -0600

    added SQL queries and CRUD opperations

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require("./api/server.js");
 
+const accountsRouter = require("./api/accounts-router")
+server.use(accountsRouter)
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
